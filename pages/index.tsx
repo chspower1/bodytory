1d6096dea2a0312d99524047640f9881d24e2d1f
chspower1 <chspower1@gmail.com>
2022-11-15 17:22:01 +0900

    chore: styled-components babel 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
+import styled from "styled-components";
 import styles from "../styles/Home.module.css";
-
+const Test = styled.div`
+  color: red;
+`;
 export default function Home() {
-  return <div>홈</div>;
+  return <Test>홈</Test>;
 }
