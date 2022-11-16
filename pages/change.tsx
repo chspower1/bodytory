9b36a8142f04261e726d567b8f56fdf8631b8f8a
KimKW1007 <face676934@gmail.com>
2022-11-16 18:45:29 +0900

    chore : 비밀번호 변경 관련

--- pages/change.tsx
+++ pages/change.tsx
@@ -1,5 +1,5 @@
 import Input from '@components/Input'
-import  { changePasswordApi } from '@libs/client/useChangePw';
+import  { changePasswordApi } from '@libs/client/accountApi';
 import { useMutation } from '@tanstack/react-query';
 import React from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form';
@@ -7,7 +7,7 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 interface PWType {
   oldPW : String;
   newPW : String;
-  newPWC ?: String;
+  newPWC : String;
 }
 
 export default function change() {
@@ -29,7 +29,9 @@ export default function change() {
     formState: { errors } } = useForm<PWType>();
   const onSubmit: SubmitHandler<PWType> = ({oldPW, newPW, newPWC}) => {
     if(newPW !== newPWC){
-      setError("newPWC", {message:"비밀번호가 일치하지 않습니다."})
+      setError("newPWC", {message:"비밀번호가 일치하지 않습니다"})
+    }else if(oldPW === newPW){
+      setError("newPW", {message:"새로운 비밀번호를 입력해주세요"})
     }else{
       changePwMutate.mutate({email: "12345", password: oldPW, newPassword: newPW})
     }
@@ -39,11 +41,11 @@ export default function change() {
     <div>
       <h3>비밀번호 변경테스트</h3>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <p><input type="text" {...register("oldPW",{ required: "필수값입니다." })} /></p>
+        <p><input type="text" {...register("oldPW",{ required: "필수값입니다" })} /></p>
         <p>{errors.oldPW && errors.oldPW.message}</p>
-        <p><input type="text" {...register("newPW",{ required: "필수값입니다." })} /></p>
+        <p><input type="text" {...register("newPW",{ required: "필수값입니다" })} /></p>
         <p>{errors.newPW && errors.newPW.message}</p>
-        <p><input type="text" {...register("newPWC",{ required: "필수값입니다." })} /></p>
+        <p><input type="text" {...register("newPWC",{ required: "필수값입니다" })} /></p>
         <p>{errors.newPWC && errors.newPWC.message}</p>
         <button type="submit">제출</button>
       </form>
