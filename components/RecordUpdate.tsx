9b6026866df19009092387a78433c30381a61416
KimKW1007 <face676934@gmail.com>
2022-11-18 17:53:28 +0900

    feat : record update 및 reocd api

---
+++ components/RecordUpdate.tsx
@@ -0,0 +1,50 @@
+import Input from '@components/Input';
+import useApi from '@libs/client/useApi';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import React from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+interface RecordUpdatePropsType{
+  recordId: string;
+  setCurrentIdx: React.Dispatch<React.SetStateAction<number>>;
+}
+interface RecordUpdateType{
+  updateWrite: string;
+}
+
+
+export default function RecordUpdate({recordId, setCurrentIdx}: RecordUpdatePropsType ) {
+  const queryClient = useQueryClient();
+  const { putApi } = useApi("/api/records/record");
+  const { mutate } = useMutation(['recordsUpdateKey'] , putApi,{
+    onSuccess(data, variables, context) {
+      queryClient.invalidateQueries(['recordsReadKey']);
+    },
+  });
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<RecordUpdateType>();
+  const onSubmit: SubmitHandler<RecordUpdateType> = ({ updateWrite }) => {
+    mutate({ id: recordId, position: "leg", description:updateWrite})
+    setCurrentIdx(-1);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+            name="updateWrite"
+            label="기록할내용"
+            type="text"
+            register={register("updateWrite", { required: "필수값입니다" })}
+            placeholder="내용을 입력하세요"
+            errorMessage={errors.updateWrite?.message}
+          />
+        <button type="submit" >수정하기</button>
+      </form>
+    </div>
+  )
+}
--- pages/api/records/record.ts
+++ pages/api/records/record.ts
@@ -2,26 +2,16 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
 import withHandler from "@libs/server/withHandler";
 import { withApiSession } from "@libs/server/withSession";
-import { WriteRecord } from "pages/write";
+
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { type, position, description }: WriteRecord = req.body;
+  const { id, createAt, type, position, description } = req.body;
   const { user } = req.session;
   if (req.method === "POST") {
-    // await client.user.update({
-    //   where:{
-    //     id : user?.id,
-    //   },
-    //   data:{
-    //     records:{
-    //       type: "user"
-    //     }
-    //   }
-    // })
     await client.record.create({
       data: {
-        type: "user",
-        position: "arm",
-        description: "test",
+        type,
+        position,
+        description,
         user: {
           connect: {
             id: user?.id,
@@ -29,7 +19,36 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
         },
       },
     });
+    return res.status(200).end()
+  }
+  if(req.method === "GET"){
+    const data = await client.record.findMany({
+      where:{
+        userId: user?.id,
+      }
+    })
+    return res.status(200).json(data)
+  }
+  if(req.method === "PUT"){
+    await client.record.update({
+      where: {
+        id
+      },
+      data: {
+        position,
+        description,
+      },
+    })
+    return res.status(200).end()
+  }
+  if(req.method === "DELETE"){
+    await client.record.delete({
+      where: {
+        id
+      }
+    })
+    return res.status(200).end()
   }
 }
 
-export default withApiSession(withHandler({ methods: ["POST", "GET", "DELETE"], handler }));
+export default withApiSession(withHandler({ methods: ["POST", "GET", "PUT", "DELETE"], handler }));
