7eb57784de7822098d477c431a1fb55cd24549ac
chspower1 <chspower1@gmail.com>
2022-11-18 14:49:04 +0900

    feat(records) : record 생성 api

--- pages/api/records/record.ts
+++ pages/api/records/record.ts
@@ -6,18 +6,30 @@ import { WriteRecord } from "pages/write";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { type, position, description }: WriteRecord = req.body;
   const { user } = req.session;
-  if(req.method === "POST"){
-    await client.user.update({
-      where:{
-        id : user?.id,
+  if (req.method === "POST") {
+    // await client.user.update({
+    //   where:{
+    //     id : user?.id,
+    //   },
+    //   data:{
+    //     records:{
+    //       type: "user"
+    //     }
+    //   }
+    // })
+    await client.record.create({
+      data: {
+        type: "user",
+        position: "arm",
+        description: "test",
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
       },
-      data:{
-        records:{
-          type: "user"
-        }
-      }
-    })
+    });
   }
 }
 
-export default withApiSession(withHandler({ methods: ["POST","GET","DELETE"], handler}));
+export default withApiSession(withHandler({ methods: ["POST", "GET", "DELETE"], handler }));
