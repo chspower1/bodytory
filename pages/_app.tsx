240aa69b18635ff886c9b992a72e9ac92dad080f
chspower1 <chspower1@gmail.com>
2022-11-21 11:55:47 +0900

    error : errorBoundary , suspens 처리 중

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,17 @@ import { QueryClient, QueryClientProvider, QueryErrorResetBoundary } from "@tans
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Layout from "@components/Layout";
 import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
 const queryClient = new QueryClient({
   defaultOptions: {
+    queries: {
+      suspense: true,
+      staleTime: 1000 * 60 * 5,
+      onError(err) {
+        console.log(err);
+      },
+    },
     mutations: {
       onError(error: any) {
         if (error.status === 400) {
@@ -23,11 +32,13 @@ declare global {
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      {/* <ErrorBoundary fallback={<div>에러</div>}> */}
       <Suspense fallback={<div>로딩중</div>}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </Suspense>
+      {/* </ErrorBoundary> */}
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
