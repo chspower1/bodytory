9ed85409c594037a0cfc6eb6fa258c00953c89eb
Ryong-E <think8867@gmail.com>
2022-11-21 16:04:07 +0900

    refactor(login.ts): 모듈화 적용

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -5,50 +5,70 @@ import { withApiSession } from "@libs/server/withSession";
 import { LoginForm } from "pages/auth/login";
 import bcrypt from "bcrypt";
 import { passwordCompare } from "utils/passwordHelper";
+
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { type } = req.body;
-  if (type === "origin") {
-    const { accountId, password }: LoginForm = req.body;
-    const foundUser = await client.user.findFirst({
-      where: {
-        accountId,
-      },
-    });
-    if (foundUser) {
-      const isPasswordCorrect = await passwordCompare(password, foundUser.password!);
-      if (isPasswordCorrect) {
-        req.session.user = {
-          id: foundUser.id,
-        };
-        await req.session.save();
-        return res.status(201).end();
-      } else {
-        return res.status(401).send("회원정보를 확인해주세요");
-      }
-    } else {
-      return res.status(401).send("회원정보를 확인해주세요");
+  let foundUser;
+  try {
+    switch (type) {
+      case "naver":
+      case "kakao":
+        foundUser = await loginBySocial(req, res);
+        break;
+      case "origin":
+      default:
+        foundUser = await loginByOrigin(req);
     }
-  }
-  if (type === "naver" || "kakao") {
-    console.log(req.body);
-    const { id, email, phone, name, birth, gender } = req.body;
-    const foundUser = await client.user.findFirst({
-      where: {
-        accountId: id + "",
-        type,
-      },
-    });
-    // 존재하는 유저
-    if (foundUser) {
-      req.session.user = {
-        id: foundUser.id,
-      };
-      await req.session.save();
-      return res.status(201).end();
+
+    if (!foundUser) {
+      throw new Error("회원정보를 확인해주세요");
     }
-    // 새로운 유저
-    else return res.status(400).json({ type, id, email, phone, name, birth, gender });
+
+    req.session.user = {
+      id: foundUser.id,
+    };
+    await req.session.save();
+
+    return res.status(201).end();
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : (<Object>error).toString();
+    return res.status(401).send(errorMessage);
   }
+}
+
+async function loginByOrigin(req: NextApiRequest) {
+  const { accountId, password }: LoginForm = req.body;
+  const foundUser = await client.user.findFirst({
+    where: {
+      accountId,
+    },
+  });
+  if (!foundUser) {
+    throw new Error("회원정보를 확인해주세요");
+  }
+
+  const isPasswordCorrect = await passwordCompare(password, foundUser.password!);
+  if (!isPasswordCorrect) {
+    throw new Error("회원정보를 확인해주세요");
+  }
+
+  return foundUser;
+}
+
+async function loginBySocial(req: NextApiRequest, res: NextApiResponse) {
+  const { id, email, phone, name, birth, gender, type } = req.body;
+  const foundUser = await client.user.findFirst({
+    where: {
+      accountId: id + "",
+      type,
+    },
+  });
+
+  if (!foundUser) {
+    res.status(400).json({ type, id, email, phone, name, birth, gender });
+  }
+
+  return foundUser;
 }
 
 export default withApiSession(withHandler({ methods: ["POST"], handler, isPrivate: false }));
