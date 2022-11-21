27f6e28c62350dbdf7669744659fdbe49e1d4aca
chspower1 <chspower1@gmail.com>
2022-11-21 11:27:59 +0900

    chore: react-error-boundary 설치

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider, QueryErrorResetBoundary } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Layout from "@components/Layout";
-const queryClient = new QueryClient();
+import { Suspense } from "react";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    mutations: {
+      onError(error: any) {
+        if (error.status === 400) {
+        }
+      },
+    },
+  },
+});
 declare global {
   interface Window {
     Kakao: any;
@@ -13,9 +23,11 @@ declare global {
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Suspense fallback={<div>로딩중</div>}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Suspense>
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
