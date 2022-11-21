ad43477c44b8dc9067f820a225109eefaf391d6c
Ryong-E <think8867@gmail.com>
2022-11-21 17:57:45 +0900

    refactor: 예외처리 추가

--- pages/api/auth/help/findId.ts
+++ pages/api/auth/help/findId.ts
@@ -17,40 +17,41 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     });
     const payload = Math.floor(10000 + Math.random() * 1000000) + "";
 
-    if (user) {
-      // 이메일 보내기
-      const mailOptions = {
-        from: process.env.MAIL_ID,
-        to: email,
-        subject: "아이디 찾기",
-        text: `인증코드 : ${payload}`,
-      };
-      const result = await smtpTransport.sendMail(mailOptions, (error, responses) => {
-        if (error) {
-          console.log(error);
-          return null;
-        } else {
-          console.log(responses);
-          return null;
-        }
-      });
-      smtpTransport.close();
-      // 토큰 생성
-      await client.certification.create({
-        data: {
-          number: payload,
-          user: {
-            connect: {
-              id: user.id,
-            },
+    if (!user) {
+      return res.status(403).send("이메일을 확인해주세요");
+    }
+    // 이메일 보내기
+    const mailOptions = {
+      from: process.env.MAIL_ID,
+      to: email,
+      subject: "아이디 찾기",
+      text: `인증코드 : ${payload}`,
+    };
+    const result = await smtpTransport.sendMail(mailOptions, (error, responses) => {
+      if (error) {
+        console.log(error);
+        return null;
+      } else {
+        console.log(responses);
+        return null;
+      }
+    });
+    smtpTransport.close();
+    // 토큰 생성
+    await client.certification.create({
+      data: {
+        number: payload,
+        user: {
+          connect: {
+            id: user.id,
           },
         },
-      });
-      console.log(payload);
-      return res.status(200).json({
-        ok: true,
-      });
-    } else return res.status(403).send("이메일을 확인해주세요");
+      },
+    });
+    console.log(payload);
+    return res.status(200).json({
+      ok: true,
+    });
   }
   if (email && token) {
     const FindToken = await client.certification.deleteMany({
@@ -63,7 +64,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       },
     });
     console.log(FindToken);
-    if(!foundUser) return res.status(403).send("회원정보를 확인해주세요");
+    if (!foundUser) return res.status(403).send("회원정보를 확인해주세요");
     if (FindToken.count > 0) return res.status(200).json({ ok: true, data: foundUser?.accountId });
     else return res.status(403).send("인증번호를 확인해주세요");
   }
