1b4c93cea8d08cf366b229d591fb0f5f94aa5ade
KimKW1007 <face676934@gmail.com>
2022-11-21 15:11:36 +0900

    chore : async await 제거

--- utils/passwordHelper.ts
+++ utils/passwordHelper.ts
@@ -2,9 +2,9 @@ import * as bcrypt from "bcrypt";
 
 const SALT = 12;
 
-export async function passwordEncryption(password: string) {
-  return await bcrypt.hash(password, SALT);
+export function passwordEncryption(password: string) {
+  return bcrypt.hash(password, SALT);
 }
-export async function passwordCompare(enterPassword: string, userPassword: string) {
-  return await bcrypt.compare(enterPassword, userPassword);
+export function passwordCompare(enterPassword: string, userPassword: string) {
+  return bcrypt.compare(enterPassword, userPassword);
 }
