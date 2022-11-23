4fed82f46a55d32abfc69e041c237aadf32885a2
KimKW1007 <face676934@gmail.com>
2022-11-23 15:14:29 +0900

    feat(RadioInput) : input type radio 컴포넌트 추가

---
+++ components/RadioInput.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { UseFormRegisterReturn } from 'react-hook-form';
+import styled from 'styled-components';
+
+export interface RadioProps {
+  label : string;
+  name : string;
+  register ?: UseFormRegisterReturn;
+  error ?: string;
+  value ?: string;
+  checked ?: boolean;
+}
+
+function RadioInput({
+  label,
+  name,
+  register,
+  error,
+  value,
+}: RadioProps) {
+  return (
+    <InputBox className={`${error ? "error" : ""}`}>
+      <Input
+        id={name}
+        type="radio"
+        value={value}
+        {...register}
+      />
+      <Label htmlFor={name}>{label}</Label>
+    </InputBox>
+  )
+}
+
+export default RadioInput
+
+const InputBox = styled.div`
+  width: 50px;
+  height:50px;
+  transition : border .3s;
+  border: 2px solid transparent;
+  background-color:${({theme})=> theme.color.input};
+  border-radius: 50%;
+  overflow:hidden;
+  user-select: none;
+  &:focus-within{
+    &.error{
+      border: 2px solid ${({theme}) => theme.color.error};
+    }
+  }
+`
+const Input = styled.input`
+  position: absolute;
+  left: -999999%;
+  &:checked + label{
+    background: #fff;
+    color: ${({theme})=> theme.color.input};
+  }
+`
+
+const Label = styled.label`
+  
+  display: block;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
