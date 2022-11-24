3d91ffc01fc918845ace72ce3797c690df22cefc
KimKW1007 <face676934@gmail.com>
2022-11-25 00:32:33 +0900

    fix(regex) : kr en regex 조건 변경

--- constant/regex.ts
+++ constant/regex.ts
@@ -1,5 +1,5 @@
 export const EMAIL_REGEX = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
 export const ACCOUNT_ID_REGEX = /^[a-zA-Z0-9]{6,}$/;
 export const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
-export const KR_EN_REGEX = /[가-힣a-zA-Z]/g;
+export const KR_EN_REGEX = /^[가-힣a-zA-Z]+$/;
 export const ONLY_KR_REGEX = /[ㄱ-ㅎㅏ-ㅣ가-힣]/g;
