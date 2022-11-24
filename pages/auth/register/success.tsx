d969d19b3a10b622ec2b7aa1841cb2f6f7d64d5f
KimKW1007 <face676934@gmail.com>
2022-11-24 23:44:32 +0900

    feat & style (success) : 구조 변경

--- pages/auth/register/success.tsx
+++ pages/auth/register/success.tsx
@@ -1,20 +1,33 @@
 import { RoundButton } from "@components/button/Button";
-import { Col, WhiteText } from "@styles/Common";
+import MessageBox from "@components/MessageBox";
+import { Col,  FlexContainer, InnerContainer, WhiteText } from "@styles/Common";
 import { theme } from "@styles/theme";
 import { NextPage } from "next";
 import Link from "next/link";
+import { FinalCommentBox } from "../help/find-id";
 
 const SuccessPage: NextPage = () => {
   return (
-    <Col>
-      <WhiteText>회원가입이 완료되었어요!</WhiteText>
-      <WhiteText>회원가입이 완료되었어요!바디토리 시작해볼까요?</WhiteText>
-      <Link href="/">
-        <RoundButton size="lg" bgColor={theme.color.mintBtn}>
-          바디토리 시작하기
-        </RoundButton>
-      </Link>
-    </Col>
+    <FlexContainer>
+      <InnerContainer>
+        <FinalCommentBox>
+          <div className="innerBox">
+            <MessageBox>
+              <p>회원가입이 완료되었어요!</p>
+              <p>바디토리를 시작해볼까요?</p>
+            </MessageBox>
+            <div className="linkButton">
+              <Link href="/">
+                <RoundButton size="lg" bgColor={theme.color.mintBtn}>
+                  바디토리 시작하기
+                </RoundButton>
+              </Link>
+            </div>
+          </div>
+        </FinalCommentBox>
+      </InnerContainer>
+    </FlexContainer>
+    
   );
 };
 export default SuccessPage;
