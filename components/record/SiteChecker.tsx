4fbcdb876c2456d8625a342bfe6d68208ac5399b
chspower1 <chspower1@gmail.com>
2022-11-27 21:22:09 +0900

    feat(SideChecker) : 버튼 추가, 로직 변경

--- components/record/SiteChecker.tsx
+++ components/record/SiteChecker.tsx
@@ -1,5 +1,5 @@
 import { RectangleButton, RoundButton } from "@components/button/Button";
-import { Col, Container, FlexContainer, ToryText } from "@styles/Common";
+import { Box, Col, Container, FlexContainer, ToryText } from "@styles/Common";
 import { useRouter } from "next/router";
 import { SiteType } from "pages/users/records/write";
 import { Dispatch, SetStateAction } from "react";
@@ -17,15 +17,15 @@ const SiteChecker = ({ hoveredSite, selectedSite, setSelectedSite }: SiteChecker
   return (
     <FlexContainer>
       <Col>
-        {selectedSite === null ? (
+        {!selectedSite ? (
           <ToryText>증상을 기록할 부위를 선택해주세요</ToryText>
         ) : (
           <ToryText>
-            <PositionText>{hoveredSite}</PositionText>에 대한 증상을 기록할까요?
+            <PositionBoxText>{hoveredSite}</PositionBoxText>에 대한 증상을 기록할까요?
           </ToryText>
         )}
 
-        {selectedSite === null ? (
+        {!selectedSite ? (
           <RectangleButton
             bgColor="rgb(208, 238, 247)"
             textColor="rgb(18, 212, 201)"
@@ -37,10 +37,21 @@ const SiteChecker = ({ hoveredSite, selectedSite, setSelectedSite }: SiteChecker
           </RectangleButton>
         ) : (
           <ButtonsBox>
-            <RoundButton width="auto" height="60px" onClick={() => router.push(`./write/${selectedSite}`)}>
+            <RoundButton
+              width="125px"
+              height="60px"
+              bgColor="rgb(83, 89, 233)"
+              onClick={() => router.push(`./write/${selectedSite}`)}
+            >
               네
             </RoundButton>
-            <RoundButton width="auto" height="60px" onClick={() => setSelectedSite(null)}>
+            <RoundButton
+              width="310px"
+              height="60px"
+              bgColor="rgb(198, 205, 250)"
+              textColor="rgb(93, 107, 178)"
+              onClick={() => setSelectedSite(null)}
+            >
               아니요, 다시 선택할래요
             </RoundButton>
           </ButtonsBox>
@@ -49,18 +60,18 @@ const SiteChecker = ({ hoveredSite, selectedSite, setSelectedSite }: SiteChecker
     </FlexContainer>
   );
 };
-const PositionText = styled.span`
+export const PositionBoxText = styled.span`
   border-radius: 10px;
   background-color: #e8e9ff;
   padding: 5px 20px;
   margin-right: 10px;
+  color: ${({ theme }) => theme.color.darkBg};
+  font-weight: 800;
 `;
-const ButtonsBox = styled.div`
-  margin-left: auto;
-  margin-right: auto;
-  width: 472px;
-  height: 60px;
-  display: flex;
+const ButtonsBox = styled(Box)`
+  margin-top: 140px;
+  width: 480px;
+  justify-content: space-between;
 `;
 
 export default SiteChecker;
