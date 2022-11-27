752a6caa840accfff23418a6f4b0a9791c523f4e
KimKW1007 <face676934@gmail.com>
2022-11-27 17:53:34 +0900

    feat(myHospital) : page 추가

---
+++ pages/users/my-hospital/clinic-list.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const ClinicList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ClinicList;
+
+const PageHead = styled.div`
+  
+`
