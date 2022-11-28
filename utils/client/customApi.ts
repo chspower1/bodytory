e41565fc082416c3cab16d437dbd3f5223839a05
DaHyunOhe <peelsob4d@gmail.com>
2022-11-28 15:10:25 +0900

    feat: customApi 테스트용 interceptor 추가

--- utils/client/customApi.ts
+++ utils/client/customApi.ts
@@ -1,46 +1,40 @@
 import axios from "axios";
 import { ErrorInfo, useState } from "react";
 
-const basicApi = () =>
-  axios.create({
+const basicApi = () => {
+  const axiosBase = axios.create({
     headers: {
       "Content-Type": "application/json",
     },
   });
 
+  axiosBase.interceptors.response.use(
+    response => response,
+    error => {
+      throw error.response;
+    },
+  );
+
+  return axiosBase;
+};
+
 export default function customApi<T = any>(url: string) {
   const postApi = async (data: T) => {
-    try {
-      const result = await basicApi().post(url, data);
-      return result.data;
-    } catch (err: any) {
-      throw err.response;
-    }
+    const result = await basicApi().post(url, data);
+    return result.data;
   };
   const getApi = async () => {
-    try {
-      const result = await basicApi().get(url);
-      return result.data;
-    } catch (err: any) {
-      throw err.response;
-    }
+    const result = await basicApi().get(url);
+    return result.data;
   };
 
   const deleteApi = async (data: T) => {
-    try {
-      const result = await basicApi().delete(url, { data });
-      return result.data;
-    } catch (err: any) {
-      throw err.response;
-    }
+    const result = await basicApi().delete(url, { data });
+    return result.data;
   };
   const putApi = async (data: T) => {
-    try {
-      const result = await basicApi().put(url, data);
-      return result.data;
-    } catch (err: any) {
-      throw err.response;
-    }
+    const result = await basicApi().put(url, data);
+    return result.data;
   };
   return { postApi, getApi, deleteApi, putApi };
 }
