8a085970e9756e94ecb8c3c932fb0d2ff8afd29d
KimKW1007 <face676934@gmail.com>
2022-11-30 14:35:55 +0900

    chore(layout) : 탈퇴페이지 예외처리

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: LayoutProps) => {
   const router = useRouter();
   const [bgColor, setBgColor] = useState("rgb(242, 243, 255)");
   useEffect(() => {
-    if (router.pathname.includes("/auth")) setBgColor("rgb(83, 89, 233)");
+    if (router.pathname.includes("/auth") && !router.pathname.includes("/withdraw")) setBgColor("rgb(83, 89, 233)");
     else setBgColor("rgb(242, 243, 255)");
   }, [router]);
   return (
