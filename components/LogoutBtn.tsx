d6de668dbe7a6125487f5572f46b1a20d9383ae0
chspower1 <chspower1@gmail.com>
2022-12-01 07:28:19 +0900

    feat(logout) : logout시 user cache값 삭제

--- components/LogoutBtn.tsx
+++ components/LogoutBtn.tsx
@@ -5,8 +5,11 @@ import { RoundButton } from "./button/Button";
 import { loggedInUser } from "atoms/atoms";
 import { useSetRecoilState } from "recoil";
 import { useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import { USER_LOGIN } from "constant/queryKeys";
 
 const LogoutBtn = () => {
+  const queryClient = useQueryClient();
   const router = useRouter();
   const setCurrentUser = useSetRecoilState(loggedInUser);
   const { deleteApi: LogoutApi } = customApi("/api/auth/logout");
@@ -15,7 +18,7 @@ const LogoutBtn = () => {
   // const { postApi: naverLogoutApi } = customApi("https://nid.naver.com/oauth2.0/token");
   const handleClickLogout = async () => {
     // console.log(localStorage.removeItem("naverToken"));
-    try{
+    try {
       const res = await axios.get("/oauth2.0/token", {
         params: {
           grant_type: "delete",
@@ -27,16 +30,17 @@ const LogoutBtn = () => {
       });
       setCurrentUser(null);
       await LogoutApi({});
-      router.push("/auth/login");      
-    }catch(err){
+      queryClient.removeQueries(USER_LOGIN);
+      router.push("/auth/login");
+    } catch (err) {
       console.log("logout Err");
     }
   };
   return (
     <>
-    <RoundButton size="md" onClick={handleClickLogout}>
-      로그아웃
-    </RoundButton>
+      <RoundButton size="md" onClick={handleClickLogout}>
+        로그아웃
+      </RoundButton>
     </>
   );
 };
