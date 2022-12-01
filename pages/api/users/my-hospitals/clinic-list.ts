6d6c2869255b8603a12ff4df200f81b7b8d8cd51
chspower1 <chspower1@gmail.com>
2022-12-01 20:44:49 +0900

    schema: relationMode="prisma" 추가

--- pages/api/users/my-hospitals/clinic-list.ts
+++ pages/api/users/my-hospitals/clinic-list.ts
@@ -4,31 +4,23 @@ import withHandler from "@utils/server/withHandler";
 import { withApiSession } from "@utils/server/withSession";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-
   if (req.method === "GET") return await myClinicList(req, res);
 
   // if (req.method === "DELETE") return await deleteHospital(req, res);
-  
 }
-
 
 async function myClinicList(req: NextApiRequest, res: NextApiResponse) {
   const { user } = req.session;
   if (!user) return res.status(401).send("회원 정보를 확인해주세요");
   const data = await client.user.findFirst({
     where: {
-      id : user.id
+      id: user.id,
     },
-    select:{
-      hospitals:{
-        include:{
-          Record:true
-        }
-      }
+    include: {
+      
     },
-    
   });
-  return res.status(200).json( data?.hospitals );
+  return res.status(200).json(data);
 }
 
 /* async function deleteHospital(req: NextApiRequest, res: NextApiResponse) {
