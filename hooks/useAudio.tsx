4574a7797253e562550ccd09269188441fa39b59
DaHyunOhe <peelsob4d@gmail.com>
2022-12-01 13:22:05 +0900

    chore: Removed test codes

--- hooks/useAudio.tsx
+++ hooks/useAudio.tsx
@@ -1,7 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
-
-import customApi from "@utils/client/customApi";
-import { useMutation } from "@tanstack/react-query";
+import { useCallback, useState } from "react";
 
 const bufferToBase64 = (buffer: any) => {
   const bytes = new Uint8Array(buffer);
@@ -91,20 +88,13 @@ const useAudio = () => {
 
   const [source, setSource] = useState<MediaStreamAudioSourceNode>();
   const [analyser, setAnalyser] = useState<ScriptProcessorNode>();
-  const [audioUrl, setAudioUrl] = useState<Blob | MediaSource>();
-  const [audioBlobUrl, setAudioBlobUrl] = useState("");
-  const [audioFile, setAudioFile] = useState<File>();
   const [audioRecognized, setAudioRecognized] = useState<string>("");
   const [isRecording, setIsRecording] = useState(false);
 
   const onRecAudio = useCallback(() => {
-    // 음원정보를 담은 노드를 생성하거나 음원을 실행또는 디코딩 시키는 일을 한다
     const audioCtx = new window.AudioContext({ sampleRate: 16000 });
-
-    // 자바스크립트를 통해 음원의 진행상태에 직접접근에 사용된다.
     const analyser = audioCtx.createScriptProcessor(0, 1, 1);
     setAnalyser(analyser);
-
     function makeSound(stream: MediaStream) {
       // 내 컴퓨터의 마이크나 다른 소스를 통해 발생한 오디오 스트림의 정보를 보여준다.
       const source = audioCtx.createMediaStreamSource(stream);
@@ -114,8 +104,6 @@ const useAudio = () => {
       source.connect(analyser);
       analyser.connect(audioCtx.destination);
     }
-
-    // 마이크 사용 권한 획득 후 녹음 시작
     navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
       const mediaRecorder = new MediaRecorder(stream, {
         audioBitsPerSecond: 16000,
@@ -125,54 +113,31 @@ const useAudio = () => {
       setStream(stream);
       setMedia(mediaRecorder);
       makeSound(stream);
-      // 음성 녹음이 시작됐을 때 onRec state값을 false로 변경
       analyser.onaudioprocess = function (e) {
         setIsRecording(true);
       };
     });
   }, []);
 
-  useEffect(() => {
-    console.log(audioUrl);
-  }, [audioUrl]);
-
   const offRecAudio = useCallback(async () => {
     setIsRecording(false);
     if (media) {
-      // dataavailable 이벤트로 Blob 데이터에 대한 응답을 받을 수 있음
       media.ondataavailable = async function (e) {
         await onSubmitAudioFile(e.data);
         setIsRecording(false);
       };
-
-      // 모든 트랙에서 stop()을 호출해 오디오 스트림을 정지
       stream?.getAudioTracks().forEach(function (track) {
         track.stop();
       });
-
-      // 미디어 캡처 중지
       media.stop();
-
-      // 메서드가 호출 된 노드 연결 해제
       analyser?.disconnect();
       source?.disconnect();
     }
   }, [media, stream, analyser, source]);
 
   const onSubmitAudioFile = async (audioUrl: Blob) => {
-    // if (audioUrl) {
-    //   const audioBlobUrl = URL.createObjectURL(audioUrl);
-    //   setAudioBlobUrl(audioBlobUrl);
-    //   console.log(audioBlobUrl); // 출력된 링크에서 녹음된 오디오 확인 가능
-    // }
     const reader = new FileReader();
     const sound = new Blob([audioUrl as BlobPart], { type: "audio/mpeg3" });
-    console.log(sound);
-    // const audioBlobUrl = URL.createObjectURL(sound);
-    // setAudioBlobUrl(audioBlobUrl);
-
-    // console.log("리더기", reader.readAsArrayBuffer(sound));
-    // const aad = new Audio(sound.toString("base64"));
     reader.readAsArrayBuffer(sound);
 
     reader.onloadend = async () => {
@@ -181,12 +146,6 @@ const useAudio = () => {
       const bufferedSound = await audioContext.decodeAudioData(arrayBuffer as ArrayBuffer);
       const resampled = await resample(bufferedSound, 16000);
       const reBlob = audioBufferToWav(resampled);
-
-      // const soundString = arrayBuffer!.toString(); // File 정보 출력
-      // console.log(soundString.slice(24));
-      console.log(reBlob);
-      // const newBase64 = arrayBuffer?.toString();
-      // console.log(newBase64?.slice(18));
 
       const PostAudio = async () => {
         const aa = await fetch("http://aiopen.etri.re.kr:8000/WiseASR/Recognition", {
@@ -205,31 +164,11 @@ const useAudio = () => {
         })
           .then(data => data.json())
           .then(json => json.return_object.recognized)
-          .then(recognized => setAudioRecognized(recognized))
-          .catch(err => console.log(err));
+          .then(recognized => setAudioRecognized(recognized));
       };
       PostAudio();
-      // mutate({ url: audioBlobUrl });
     };
   };
-
-  // const RecordBtn = () => {
-  //   return (
-  //     <div style={{ margin: "400px" }}>
-  //       <button onClick={isRecording ? offRecAudio : onRecAudio}>녹음</button>
-  //       <div>{isRecording ? "녹음중" : "대기"}</div>
-  //       <button onClick={onSubmitAudioFile}>결과 확인</button>
-  //       <audio controls src={audioBlobUrl} />
-
-  //       {/* <p>Microphone: {listening ? "on" : "off"}</p>
-  //       <button onClick={() => (listening ? SpeechRecognition.stopListening() : SpeechRecognition.startListening())}>
-  //         {listening ? "End" : "Start"}
-  //       </button>
-  //       <button onClick={() => resetTranscript()}>Reset</button>
-  //       <p>{transcript}</p> */}
-  //     </div>
-  //   );
-  // };
   return { offRecAudio, onRecAudio, onSubmitAudioFile, audioRecognized, isRecording };
 };
 
