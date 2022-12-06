c33001c8f4b3b9af56dc9b9e7a14c40e672c8d05
KimKW1007 <face676934@gmail.com>
2022-12-06 22:35:47 +0900

    fix(hospitalSelectPart) : removeQueries 추가

--- next/components/hospital/HospitalSelectPart.tsx
+++ next/components/hospital/HospitalSelectPart.tsx
@@ -1,18 +1,23 @@
 import BodyNavigator from '@components/records/BodyNavigator';
 import { BackButton, GuideText, SelectBodyPartProps, SelectPartContainer, SelectPartWarp } from '@components/records/SelectBodyPart';
+import { useQueryClient } from '@tanstack/react-query';
+import { RECORDS_READ } from 'constant/queryKeys';
 import { useRouter } from 'next/router';
 import React from 'react'
 
 const HospitalSelectPart = ({ selectedBodyPart, setSelectedBodyPart}: SelectBodyPartProps) => {
   const router = useRouter();
+  const queryClient = useQueryClient();
+  const handleClickResetQuerys = () =>{
+    queryClient.removeQueries([RECORDS_READ])
+    router.push("/hospital");
+  }
   return (
     <SelectPartWarp>
       <SelectPartContainer>
         {
           router.pathname !== "/hospital" && (
-            <BackButton onClick={() => {
-                router.push("/hospital");
-              }}>
+            <BackButton onClick={handleClickResetQuerys}>
               <span>환자리스트</span>
             </BackButton>
           )
