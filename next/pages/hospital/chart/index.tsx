c16a80e2ebba8b276c58e574af0cacf9e0e9626a
KimKW1007 <face676934@gmail.com>
2022-12-06 22:15:08 +0900

    chore(hospital-Chart) : 불필요한 props 제거

--- next/pages/hospital/chart/index.tsx
+++ next/pages/hospital/chart/index.tsx
@@ -6,10 +6,11 @@ import { bodyPartType } from 'types/bodyParts';
 
 const HospitalChartHomePage = () => {
   const [selectedBodyPart, setSelectedBodyPart] = useState<bodyPartType>(null);
+  console.log(selectedBodyPart)
   return (
     <RecordWrap>
       <HospitalSelectPart selectedBodyPart={selectedBodyPart} setSelectedBodyPart={setSelectedBodyPart} />
-      <HospitalChart selectedBodyPart={selectedBodyPart} />
+      <HospitalChart  />
     </RecordWrap>
   )
 }
