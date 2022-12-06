0449275f9c94fc6a71384369e05866305461b7df
KimKW1007 <face676934@gmail.com>
2022-12-06 22:14:47 +0900

    chore(hospital-login) : 불필요한 styledComponents 제거 및 Title 추가

--- next/pages/hospital/login.tsx
+++ next/pages/hospital/login.tsx
@@ -19,6 +19,7 @@ import styled from "styled-components";
 import MessageBox from "@components/MessageBox";
 import { loggedInHospital } from "atoms/atoms";
 import { useSetRecoilState } from "recoil";
+import { LoginInputAreaBox } from "pages/auth/login";
 
 export interface LoginForm {
   accountId: string;
@@ -67,15 +68,19 @@ const LoginPage: NextPage = () => {
 
   return (
     <FlexContainer>
-      <InnerContainer>
+      <HospitalInnerContainer>
+        <TitleBox>
+          <h2>병원 관리자 전용</h2>
+        </TitleBox>
         <MessageBox isErrorsMessage={isErrorsMessage}>
           {isErrorsMessage === undefined &&
-            (isError ? <>앗! 로그인 정보를 다시 한번 확인해주세요</> : <>로그인 정보를 입력해주세요</>)}
+            (isError ?  "앗! 로그인 정보를 다시 한번 확인해주세요" : "로그인 정보를 입력해주세요")}
         </MessageBox>
         <LoginForm as="form" onSubmit={handleSubmit(onValid)}>
           <LoginFormInnerBox>
             <LoginInputAreaBox>
               <Input
+                light
                 name="accountId"
                 register={register("accountId", {
                   required: "아이디를 입력해주세요",
@@ -85,6 +90,7 @@ const LoginPage: NextPage = () => {
                 motion={false}
               />
               <Input
+                light
                 name="password"
                 type="password"
                 register={register("password", {
@@ -94,85 +100,36 @@ const LoginPage: NextPage = () => {
                 error={errors.password || isError}
                 motion={false}
               />
-              {/* <Input
-            name="autoLogin"
-            label="자동로그인"
-            type="checkbox"
-            register={register("autoLogin")}
-            errorMessage={errors.password?.message}
-          /> */}
             </LoginInputAreaBox>
             <RoundButton size="lg" bgColor={theme.color.mintBtn} disable={!isCompletion}>
               로그인
             </RoundButton>
           </LoginFormInnerBox>
         </LoginForm>
-        <LoginFindBox>
-          <Link href="/auth/help/find-id">
-            <WhiteText>아이디 찾기</WhiteText>
-          </Link>
-          <i>|</i>
-          <Link href="/auth/help/find-pw">
-            <WhiteText>비밀번호 찾기</WhiteText>
-          </Link>
-        </LoginFindBox>
-      </InnerContainer>
+      </HospitalInnerContainer>
     </FlexContainer>
   );
 };
 export default LoginPage;
 
-export const ToryTextBox = styled.div`
-  text-align: center;
-  padding: 50px 0 65px;
-  letter-spacing: -0.6px;
-  word-spacing: -4px;
-`;
-
+const HospitalInnerContainer =styled(InnerContainer)`
+  .messageBox{
+    color: #000;
+  }
+`
+const TitleBox =styled.div`
+  margin-bottom: 20px;
+  h2{
+    text-align:center;
+    font-size: 34px;
+    font-weight: 700;
+  }
+`
 const LoginForm = styled.form`
   display: flex;
   justify-content: center;
 `;
-export const LoginInputAreaBox = styled.div`
-  margin-bottom: 40px;
-`;
 const LoginFormInnerBox = styled.div`
   display: inline-block;
 `;
-const LoginFindBox = styled(Row)`
-  padding: 20px 0;
-  margin: 0 0 70px;
-  a {
-    margin: 0 12px;
-    span {
-      font-size: 15px;
-    }
-  }
-  i {
-    color: #fff;
-    user-select: none;
-  }
-`;
-const SocialLoginBox = styled(Row)`
-  margin: 0 0 30px;
-  .soscialInnerBox {
-    display: flex;
-    > div,
-    > button {
-      margin: 0 20px;
-      div {
-        font-size: 16px;
-      }
-    }
-  }
-`;
 
-const RegisterLinkBox = styled(Row)`
-  padding: 20px 0;
-  > span {
-    font-size: 15px;
-  }
-  a {
-    margin: 0 0 0 10px;
-  }
-`;
