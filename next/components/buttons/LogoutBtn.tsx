8645d0d26a6955beb649c8490ddace47259d3782
KimKW1007 <face676934@gmail.com>
2022-12-06 22:07:58 +0900

    chore(hospital) : header 관련 로그아웃 설정

--- next/components/buttons/LogoutBtn.tsx
+++ next/components/buttons/LogoutBtn.tsx
@@ -1,29 +1,31 @@
 import customApi from "utils/client/customApi";
 import { useRouter } from "next/router";
 import { RoundButton } from "./Button";
-import { loggedInUser } from "atoms/atoms";
+import { loggedInHospital, loggedInUser } from "atoms/atoms";
 import { useSetRecoilState } from "recoil";
 import { useQueryClient } from "@tanstack/react-query";
-import { USER_LOGIN } from "constant/queryKeys";
+import { HOSPITAL_LOGIN, USER_LOGIN } from "constant/queryKeys";
 
-const LogoutBtn = () => {
+const LogoutBtn = ({isHospital}:{isHospital :boolean;}) => {
   const queryClient = useQueryClient();
   const router = useRouter();
   const setCurrentUser = useSetRecoilState(loggedInUser);
+  const setCurrentHospital = useSetRecoilState(loggedInHospital);
   const { deleteApi: LogoutApi } = customApi("/api/auth/logout");
   const handleClickLogout = async () => {
     try {
       setCurrentUser(null);
-      queryClient.removeQueries({ queryKey: USER_LOGIN, exact: true });
+      setCurrentHospital(null);
+      queryClient.removeQueries({ queryKey: isHospital ? HOSPITAL_LOGIN :USER_LOGIN, exact: true });
       await LogoutApi({});
-      router.push("/auth/login");
+      router.push(isHospital ? "/hospital/login" :"/auth/login");
     } catch (err) {
       console.log("logout Err");
     }
   };
   return (
     <>
-      <RoundButton size="md" onClick={handleClickLogout}>
+      <RoundButton size={isHospital ? "sm" : "md"} onClick={handleClickLogout}>
         로그아웃
       </RoundButton>
     </>
