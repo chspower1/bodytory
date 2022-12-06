eb5298a11666129ca17c090ec2006563eedaacf9
chspower1 <chspower1@gmail.com>
2022-12-06 20:45:36 +0900

    feat(useHospital) :handleClick 함수 추가

--- next/hooks/useHospital.tsx
+++ next/hooks/useHospital.tsx
@@ -1,9 +1,14 @@
+import { RectangleButton } from "@components/buttons/Button";
+import { theme } from "@styles/theme";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import customApi from "@utils/client/customApi";
 import { HOSPITALS } from "constant/queryKeys";
+import { SetStateAction, useState } from "react";
 
 const useAddHospital = () => {
   const queryClient = useQueryClient();
+  const [showModal, setShowModal] = useState(false);
+  const [onConnected, setOnConnected] = useState(false);
   const { getApi, postApi, putApi, deleteApi } = customApi("/api/users/my-hospitals");
   const refreshHospitalCache = () => {
     queryClient.invalidateQueries(["isMyHospital"]);
@@ -24,7 +29,25 @@ const useAddHospital = () => {
       refreshHospitalCache();
     },
   });
+  const handleClickAddHospital = (id: number, option?: (value: SetStateAction<boolean>) => void) => {
+    addHospitalMutate({ id });
+    option && option(true);
+    setShowModal(false);
+  };
+  const handleClickDeleteHospital = (id: number, option?: (value: SetStateAction<boolean>) => void) => {
+    deleteHospitalMutate({ id });
+    option && option(false);
+    setShowModal(false);
+  };
 
-  return { addHospitalMutate, deleteHospitalMutate, sharedHospitalMutate };
+  return {
+    addHospitalMutate,
+    deleteHospitalMutate,
+    sharedHospitalMutate,
+    showModal,
+    setShowModal,
+    handleClickAddHospital,
+    handleClickDeleteHospital,
+  };
 };
 export default useAddHospital;
