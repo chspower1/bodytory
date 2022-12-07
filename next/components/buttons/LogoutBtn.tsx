da89d4672ac46cb90359ad468fbfd3e2ac7e7d3c
KimKW1007 <face676934@gmail.com>
2022-12-07 21:41:01 +0900

    chore(logoutBtn) : 로그아웃시 persist null 추가

--- next/components/buttons/LogoutBtn.tsx
+++ next/components/buttons/LogoutBtn.tsx
@@ -1,7 +1,7 @@
 import customApi from "utils/client/customApi";
 import { useRouter } from "next/router";
 import { RoundButton } from "./Button";
-import { loggedInHospital, loggedInUser } from "atoms/atoms";
+import { currentPatientInfo, loggedInHospital, loggedInUser } from "atoms/atoms";
 import { useSetRecoilState } from "recoil";
 import { useQueryClient } from "@tanstack/react-query";
 import { HOSPITAL_LOGIN, USER_LOGIN } from "constant/queryKeys";
@@ -11,11 +11,16 @@ const LogoutBtn = ({isHospital}:{isHospital ?:boolean;}) => {
   const router = useRouter();
   const setCurrentUser = useSetRecoilState(loggedInUser);
   const setCurrentHospital = useSetRecoilState(loggedInHospital);
+  const setCurrentPatientInfo = useSetRecoilState(currentPatientInfo);
   const { deleteApi: LogoutApi } = customApi("/api/auth/logout");
   const handleClickLogout = async () => {
     try {
       setCurrentUser(null);
       setCurrentHospital(null);
+      setCurrentPatientInfo({
+        name : "",
+        id: null
+      });
       queryClient.removeQueries({ queryKey: isHospital ? [HOSPITAL_LOGIN] : [USER_LOGIN] });
       await LogoutApi({});
       router.push(isHospital ? "/hospital/login" :"/auth/login");
