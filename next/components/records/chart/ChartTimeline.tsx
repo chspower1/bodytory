568e02ea81b6193a9aa6747fc8604f8b4faae121
Ryong-E <think8867@gmail.com>
2022-12-07 20:56:46 +0900

    fix: 스켈레톤UI가 먼저뜨지않는 부분 수정

--- next/components/records/chart/ChartTimeline.tsx
+++ next/components/records/chart/ChartTimeline.tsx
@@ -154,13 +154,13 @@ const ChartTimeline = () => {
                 <strong>{KoreanPosition[position!]}</strong>에 대한 기록이 없습니다
               </p>
             </NoRecord>
+          ) : isLoading ? (
+            <RecordSkeleton />
           ) : !query.position ? (
             <NoRecord>
               <img src={ToriQuestion.src} />
               <p>자세한 기록을 확인하고 싶은 부위를 선택해주세요</p>
             </NoRecord>
-          ) : isLoading ? (
-            <RecordSkeleton />
           ) : (
             filtredRecord?.map((record, index) => (
               <RecordBox key={index}>
