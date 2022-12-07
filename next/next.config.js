f02eceb99e8cb04b2144aa963b2ffa6b243fac36
DaHyunOhe <peelsob4d@gmail.com>
2022-12-07 22:29:37 +0900

    fix: next.config.js 문법 수정

--- next/next.config.js
+++ next/next.config.js
@@ -1,12 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
-  output: "standalone",
-  compiler: {
-    styledComponents: true,
-  },
-  webpack(config) {
+module.exports = {
+  webpack: config => {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
