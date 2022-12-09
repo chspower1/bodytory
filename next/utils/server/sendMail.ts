0d0a407b5abe867e9346388dc2d31d796116a746
KimKW1007 <face676934@gmail.com>
2022-12-09 22:21:45 +0900

    feat(sendMail) : html로 보내기

--- next/utils/server/sendMail.ts
+++ next/utils/server/sendMail.ts
@@ -6,13 +6,16 @@ const sendMail = (email: string, payload: string, subject: string) => {
   const mailOptions: MailOptions = {
     from: process.env.MAIL_ID,
     to: email,
-    subject,
+    subject : "바디토리에서 인증번호가 왔어요!",
     // text: `인증코드 : ${payload}`,
     html: `
     <div style="width: 100%; height: 100%; display: flex;" >
-      <div style="flex-shrink:0; position:relative; margin: auto; width: 1000px; height: 454px;box-sizing: content-box; padding-right: 143px;">
-        <div style="width: 100%; height: 100%; background: url(https://toriai.s3.ap-northeast-2.amazonaws.com/uploads/email_authentication.png) no-repeat center center; background-size: contain;"></div>
-        <span style="position:absolute; left:50%; bottom:42px; transform:translateX(-50%); font-size:30px; font-weight:900; color:#4B50D3; letter-spacing:6px; ">123456</span>
+      <div style="flex-shrink:0; position:relative; margin: auto; width: 1000px; height: 454px;box-sizing: content-box; ">
+        <div style="display: flex; width: 100%; height: 100%; background: url(https://toriai.s3.ap-northeast-2.amazonaws.com/uploads/email_authentication.png) no-repeat center center; background-size: contain;">
+          <div style="margin: auto 287px 49px auto; width:280px; height:54px; display: flex;">
+            <span style="font-size:30px; font-weight:900; color:#4B50D3; letter-spacing:6px; margin: auto; ">${payload}</span>
+          </div>
+        </div>
       </div>
     </div>`,
   };
