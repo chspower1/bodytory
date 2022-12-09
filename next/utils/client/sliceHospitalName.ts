723dac25a5aee2788a65311c9291f99219fdf81f
Ryong-E <think8867@gmail.com>
2022-12-09 12:50:37 +0900

    fix: 이미지 업로드 파일명 수정/만나이 오류 수정/병원이름자르는기능 수정

--- next/utils/client/sliceHospitalName.ts
+++ next/utils/client/sliceHospitalName.ts
@@ -1,13 +1,32 @@
+const EXCEOPTION_WORD = ["재단", "협회", "본부", "센타", "자치도지부"];
 const sliceName = (name: string) => {
-  const split = name.split(" ");
+  let sliceResult = name.split(" ");
 
-  if (split.length > 1) return split.filter(elem => elem.includes("병원"));
+  if (sliceResult.length > 1) {
+    sliceResult = sliceResult.filter(word => {
+      if (word.includes("병원") || word.includes("의원") || word.includes("의료원")) return true;
+    });
+  }
 
-  const resplit = split.join("").split("재단");
+  // const resplit = split.join("").split("재단").join("").split("협회").join("").split("본부");
+  EXCEOPTION_WORD.forEach(word => {
+    sliceResult.forEach(elem => {
+      if (elem.includes(word)) {
+        sliceResult = seperation(sliceResult, word);
+      }
+    });
+    // if (sliceResult[0].includes(word)) {
+    //   sliceResult = pipe(sliceResult, word);
+    // }
+  });
 
-  if (resplit.length === 1) return resplit;
+  if (sliceResult.length === 1) return sliceResult;
 
-  return resplit[1];
+  return sliceResult[1];
 };
+
+function seperation(word: string[], splitword: string) {
+  return word.join("").split(splitword);
+}
 
 export default sliceName;
