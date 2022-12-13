012925de999830d4df194dbfe95ad17d63937116
DaHyunOhe <peelsob4d@gmail.com>
2022-12-13 15:30:49 +0900

    fix: prod 환경에서 lottie 에러 임시수정

--- next/components/lotties/ToryPurpleAnim.tsx
+++ next/components/lotties/ToryPurpleAnim.tsx
@@ -3,14 +3,12 @@ import ToryPurpleAnimation from "@src/assets/lotties/tory_purple.json";
 import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
-
 interface ToryAnimProps {
   toryMotionIdx: number;
   width: number;
 }
 
-const ToryPurpleAnim = ( { toryMotionIdx, width }: ToryAnimProps ) => {
-
+const ToryPurpleAnim = ({ toryMotionIdx, width }: ToryAnimProps) => {
   const [ready, setReady] = useState<boolean>(false);
   const [toryPurple, setToryPurple] = useState<AnimationItem>();
 
@@ -18,41 +16,33 @@ const ToryPurpleAnim = ( { toryMotionIdx, width }: ToryAnimProps ) => {
 
   // const frameSegments: AnimationSegment[] = [ [0, 149], [150, 215], [216, 276], [277, 456], [457, 576], [577, 725] ];
 
-
   useEffect(() => {
-    setToryPurple(lottie.loadAnimation({
-      container: lottieRef.current,
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      animationData: ToryPurpleAnimation
-    }));
+    setToryPurple(
+      lottie.loadAnimation({
+        container: lottieRef.current,
+        renderer: "canvas",
+        loop: true,
+        autoplay: true,
+        animationData: ToryPurpleAnimation,
+      }),
+    );
 
     setReady(true);
-
   }, []);
-  
-
 
   useEffect(() => {
-
-    if(toryPurple) {
-      toryPurple
+    if (toryPurple) {
+      toryPurple;
     }
 
     console.log(toryMotionIdx);
-
   }, [ready]);
 
-
-  return (
-    <LottieElem ref={lottieRef} width={width} />
-  );
+  return <LottieElem ref={lottieRef} width={width} />;
 };
 
-const LottieElem = styled.div<{width: number}>`
-  width: ${ ({ width }) => width }px;
+const LottieElem = styled.div<{ width: number }>`
+  width: ${({ width }) => width}px;
 `;
-
 
 export default ToryPurpleAnim;
