6f5df6b2c5732b744af26f700cbeb1c9378b9d1d
chspower1 <chspower1@gmail.com>
2022-12-16 00:42:08 +0900

    chore(sendMail) : sendMail 주석 제거

--- next/utils/server/sendMail.ts
+++ next/utils/server/sendMail.ts
@@ -3,30 +3,30 @@ import { MailOptions } from "nodemailer/lib/sendmail-transport";
 import email_authentication from "@public/static/email_authentication.png";
 
 const sendMail = (email: string, payload: string, subject: string) => {
-  // const mailOptions: MailOptions = {
-  //   from: process.env.MAIL_ID,
-  //   to: email,
-  //   subject: "바디토리에서 인증번호가 왔어요!",
-  //   // text: `인증코드 : ${payload}`,
-  //   html: `
-  //   <div style="width: 100%; height: 100%; display: flex;" >
-  //     <div style="flex-shrink:0; position:relative; margin: auto; width: 1000px; height: 454px;box-sizing: content-box; ">
-  //       <div style="display: flex; width: 100%; height: 100%; background: url(https://toriai.s3.ap-northeast-2.amazonaws.com/uploads/email_authentication.png) no-repeat center center; background-size: contain;">
-  //         <div style="margin: auto 287px 49px auto; width:280px; height:54px; display: flex;">
-  //           <span style="font-size:30px; font-weight:900; color:#4B50D3; letter-spacing:6px; margin: auto; ">${payload}</span>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>`,
-  // };
-  // smtpTransport.sendMail(mailOptions, (error, responses) => {
-  //   if (error) {
-  //     return null;
-  //   } else {
-  //     return null;
-  //   }
-  // });
-  // smtpTransport.close();
+  const mailOptions: MailOptions = {
+    from: process.env.MAIL_ID,
+    to: email,
+    subject: "바디토리에서 인증번호가 왔어요!",
+    // text: `인증코드 : ${payload}`,
+    html: `
+    <div style="width: 100%; height: 100%; display: flex;" >
+      <div style="flex-shrink:0; position:relative; margin: auto; width: 1000px; height: 454px;box-sizing: content-box; ">
+        <div style="display: flex; width: 100%; height: 100%; background: url(https://toriai.s3.ap-northeast-2.amazonaws.com/uploads/email_authentication.png) no-repeat center center; background-size: contain;">
+          <div style="margin: auto 287px 49px auto; width:280px; height:54px; display: flex;">
+            <span style="font-size:30px; font-weight:900; color:#4B50D3; letter-spacing:6px; margin: auto; ">${payload}</span>
+          </div>
+        </div>
+      </div>
+    </div>`,
+  };
+  smtpTransport.sendMail(mailOptions, (error, responses) => {
+    if (error) {
+      return null;
+    } else {
+      return null;
+    }
+  });
+  smtpTransport.close();
 };
 
 export default sendMail;
