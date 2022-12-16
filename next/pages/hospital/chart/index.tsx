331f1b438f38aecc1810234f7c7ca20bf2801488
KimKW1007 <face676934@gmail.com>
2022-12-16 10:47:03 +0900

    chore(hospitalmodal) : width auto로 변경

--- next/pages/hospital/chart/index.tsx
+++ next/pages/hospital/chart/index.tsx
@@ -8,8 +8,4 @@ const HospitalChartHomePage: NextPage = () => {
 };
 
 export default HospitalChartHomePage;
-export const getServerSideProps = withGetServerSideProps(async (context: GetServerSidePropsContext) => {
-  return {
-    props: {},
-  };
-});
+
